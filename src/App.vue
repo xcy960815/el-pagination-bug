f3a60cf7fb3f1702d1b1eb1d8f6f3dcf27757034
xcy960815 <18763006837@163.com>
2024-11-04 23:15:26 +0800

    feat: el-pagination bug demo

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-button @click="handleSetTotalUndefined">SetTotalUndefined</el-button>
+  <el-pagination
+    v-model:current-page="currentPage"
+    v-model:page-size="pageSize4"
+    :page-sizes="[10, 20, 30, 40]"
+    layout="total, sizes, prev, pager, next, jumper"
+    :total="total"
+    @size-change="handleSizeChange"
+    @current-change="handleCurrentChange"
+  />
+</template>
+
+<script lang='ts' setup>
+import { ref } from "vue";
+
+const currentPage = ref(1);
+const pageSize4 = ref(0);
+const total = ref<number | undefined>(400);
+
+const handleSizeChange = (val: number) => {
+  console.log(`${val} items per page`);
+};
+
+const handleCurrentChange = (val: number) => {
+  // 当点击 handleSetTotalUndefined 方法后，不应该触发 handleCurrentChange 方法
+  console.log(`current page: ${val}`);
+};
+
+const handleSetTotalUndefined = () => {
+  total.value = undefined;
+};
+</script>
+<style lang='less' scoped>
+</style>
